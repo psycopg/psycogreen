5533bc0ade45c889460f9a1df19845795f7cbe41
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2010-04-08 10:16:53 +0100

    Show Psycopg version if not compatible.

--- eventlet/psyco_eventlet.py
+++ eventlet/psyco_eventlet.py
@@ -33,7 +33,8 @@ def make_psycopg_green():
     """Configure Psycopg to be used with eventlet in non-blocking way."""
     if not hasattr(extensions, 'set_wait_callback'):
         raise ImportError(
-            "Support for coroutines is available only from Psycopg 2.2.0")
+            "support for coroutines not available in this Psycopg version (%s)"
+            % psycopg2.__version__)
 
     extensions.set_wait_callback(eventlet_wait_callback)
 
